9e439a2b7ceb255ba5240c1867e374c194e59fba
Marcos Vinicius <marcos.viniciuswac@outlook.com>
2022-05-02 13:32:16 -0300

    Criando novo exercicio

---
+++ 2 - PageObjects/TradePathPage.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestesFuncionaisComSelenium._2___PageObjects
+{
+    public class TradePathPage
+    {
+        private DLS dls;
+
+        public TradePathPage(WebDriver driver) 
+        {
+            dls = new DLS(driver);
+        }
+
+        public void ClicarElemento(string xpath) 
+        {
+            dls.DeveUtilizarAEsperaImplicita();
+            dls.CadastarButtonXPath(xpath);
+        }
+
+        public void ClicarNoCombo(string xpath, string valor) 
+        {
+            dls.DeveUtilizarAEsperaImplicita();
+            dls.PreencherCombo(xpath, valor);
+        }
+    }
+}
